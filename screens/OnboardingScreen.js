d44ec27c453e791d3dad1af1719642d3db960829
SANKET7738 <sanket.mhatre@vit.edu.in>
2021-05-05 01:36:14 +0530

    project intialized

---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
+const OnboardingScreen = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            <Button
+                title="Click Here"
+                onPress={() => navigation.navigate("Login")}
+            />
+        </View>
+    );
+};
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
