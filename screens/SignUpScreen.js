93dda3de5880ccc631e5ad6f8f4cd1827d73d2f2
SANKET7738 <sanket.mhatre@vit.edu.in>
2021-05-13 03:20:30 +0530

    added form validation with yup

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -4,6 +4,27 @@ import FormButton from '../components/FormButton';
 import FormInput from '../components/FormInput';
 import SocialButton from '../components/SocialButton';
 import { Formik } from 'formik';
+import * as yup from 'yup';
+
+const signUpSchema = yup.object({
+    name: yup.string()
+        .required("Name is required"),
+    phoneNo: yup.string()
+        .required('Phone number is required'),
+    email: yup.string()
+        .required("Email Address is required")
+        .email("Please enter valid email"),
+    password: yup.string()
+        .matches(/\w*[a-z]\w*/,  "Password must have a small letter")
+        .matches(/\w*[A-Z]\w*/,  "Password must have a capital letter")
+        .matches(/\d/, "Password must have a number")
+        .matches(/[!@#$%^&*()\-_"=+{}; :,<.>]/, "Password must have a special character")
+        .min(8, ({ min }) => `Password must be at least ${min} characters`)
+        .required("Password is required"),
+    confirmPassword: yup.string()
+        .oneOf([yup.ref('password')], 'Passwords do not match')
+        .required('Confirm password is required'),
+})
 
 const SignUpScreen = ({navigation}) => {
     const [ userData, setUserData ] = useState([])
@@ -23,6 +44,7 @@ const SignUpScreen = ({navigation}) => {
             <View>
                 <TouchableWithoutFeedback onPress={Keyboard.dismiss()}>
                     <Formik
+                        validationSchema={signUpSchema}
                         initialValues={{ 
                             name: '', 
                             phoneNo: '',
@@ -43,14 +65,22 @@ const SignUpScreen = ({navigation}) => {
                                     onChangeText={formikProps.handleChange('name')}
                                     placeholderText={"Name"}
                                     placeholderTextColor="black"
+                                    onBlur={formikProps.handleBlur('name')}
                                 />
+                                {formikProps.touched.name && formikProps.errors.name ? (
+                                    <Text style={styles.errorMsg}>{formikProps.errors.name}</Text>
+                                ) : null}
                                 <FormInput
                                     labelValue={formikProps.values.phoneNo}
                                     onChangeText={formikProps.handleChange('phoneNo')}
                                     placeholderText={"Mobile Number"}
                                     placeholderTextColor="black"
                                     keyboardType="number-pad"
+                                    onBlur={formikProps.handleBlur('phoneNo')}
                                 />
+                                {formikProps.touched.phoneNo && formikProps.errors.phoneNo ? (
+                                    <Text style={styles.errorMsg}>{formikProps.errors.phoneNo}</Text>
+                                ) : null}
                                 <FormInput
                                     labelValue={formikProps.values.email}
                                     onChangeText={formikProps.handleChange('email')}
@@ -59,21 +89,33 @@ const SignUpScreen = ({navigation}) => {
                                     keyboardType="email-address"
                                     autoCapitalize="none"
                                     autoCorrect={false}
+                                    onBlur={formikProps.handleBlur('email')}
                                 />
+                                {formikProps.touched.email && formikProps.errors.email ? (
+                                    <Text style={styles.errorMsg}>{formikProps.errors.email}</Text>
+                                ) : null}
                                 <FormInput
                                     labelValue={formikProps.values.password}
                                     onChangeText={formikProps.handleChange('password')}
                                     placeholderText={"Password"}
                                     placeholderTextColor="black"
                                     secureTextEntry={true}
+                                    onBlur={formikProps.handleBlur('password')}
                                 />
+                                {formikProps.touched.password && formikProps.errors.password ? (
+                                    <Text style={styles.errorMsg}>{formikProps.errors.password}</Text>
+                                ) : null}
                                 <FormInput
                                     labelValue={formikProps.values.confirmPassword}
                                     onChangeText={formikProps.handleChange('confirmPassword')}
                                     placeholderText={"Confirm Password"}
                                     placeholderTextColor="black"
                                     secureTextEntry={true}
+                                    onBlur={formikProps.handleBlur('confirmPassword')}
                                 />
+                                {formikProps.touched.confirmPassword && formikProps.errors.confirmPassword ? (
+                                    <Text style={styles.errorMsg}>{formikProps.errors.confirmPassword}</Text>
+                                ) : null}
                                 <View style={styles.SignUp}>
                                     <FormButton
                                         onPress={formikProps.handleSubmit}
@@ -137,5 +179,8 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: 'bold',
         color: '#F50057'
+    },
+    errorMsg: {
+        color: 'red',
     }
 })
